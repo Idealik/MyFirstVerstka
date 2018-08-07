93acb184c0ad2524c3ca327984aa283717b932b9
Марк <shavl.mark@yandex.ru>
2018-08-07 13:04:10 +0300

    Мой первый сайт для отца, все еше в доработке

---
+++ script.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.toggles button').click(function(){
+        var get_id = this.id;
+        var get_current = $('.posts .' + get_id);
+
+        $('.post').not(get_current).hide(500);
+        get_current.show(500);
+    });
+
+    $('#showall').click(function(){
+        $('.post').show(500);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:8
+    });
+  });
+
+  if($(window).width()<420){
+    $(".owl-carousel").owlCarousel({
+        items:3});
+    };
+  
+    $(document).ready(function () {
+        $("a").click(function () {
+            var elementClick = $(this).attr("href");
+            var destination = $(elementClick).offset().top;
+                $('html, body').animate({ scrollTop: destination }, 700); //1100 - скорость   
+            return false;
+        });
+    });
+
+    // header
